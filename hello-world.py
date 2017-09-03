3da08aeb3fff78c52fcba23959a6873174eef6a1
hone5t <hone5t@hone5t.com>
2017-09-02 22:36:17 -0500

    separate data and make the exmaple more dynamic

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,30 @@
 #!venv/bin/python
+'''
+Learn 
+'''
 from sklearn import tree
-features = [[140, 1], [131, 1], [150, 0], [170, 0]]
-labels = [0, 0, 1, 1]
+import csv
+
+with open('data/apple-orange.csv') as data_file:
+  next(data_file,None)
+  rows = csv.reader(data_file,delimiter=',')
+  features = []
+  labels = []
+  for row in rows:
+    isBumpy = 1 if row[1] == "Bumpy" else 0
+    isOrange = 1 if row[2] == "Orange" else 0
+    features.append([int(row[0]),isBumpy])
+    labels.append(isOrange)
+
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-print (clf.predict([[170,1]]))
+try:
+  weight  = int(input('please enter item weight ex:150\n'))
+except:
+  print("please enter integer only. exiting now")
+
+surface = input('please enter what the surface look like Bumpy or Smooth:\n')
+surface = 1 if surface == "Bumpy" else 0
+prediction = clf.predict([[weight,surface]])
+result = "Orange" if prediction == 1 else "Apple"
+print("based on your input the item is: "+result)
